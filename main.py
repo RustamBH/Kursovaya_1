132f7fd135283cb01ebf30a96cbd69d9b4e67af5
RustamBH <kamaji@mail.ru>
2022-03-21 21:09:09 +0400

    modified main.py

--- main.py
+++ main.py
@@ -66,22 +66,22 @@ def get_profile_photos(usr_items_list):
     max_profile_photo_list = []
     user_params_list = []
     for item in usr_items_list:
-        photo_params_dict = {}        
+        photo_params_dict = {}
+        max_size = 1
         photo_params_dict['id'] = item['id']
         photo_params_dict['date'] = item['date']
         photo_params_dict['user_likes'] = item['likes']['count']
         user_params_list.append(item['likes']['count'])
         photo_params_list = item['sizes']
 
-        max_size = max([photo_sz_dict['height'] * photo_sz_dict['width'] for photo_sz_dict in photo_params_list
-                        if photo_sz_dict['height'] > 0 and photo_sz_dict['width'] > 0])
-        for photo_param in photo_params_list:
-            if photo_param['height'] > 0 and photo_param['width'] > 0:
-                if photo_param['height'] * photo_param['width'] == max_size:                    
-                    photo_params_dict['url'] = photo_param['url']
-                    photo_params_dict['height'] = photo_param['height']
-                    photo_params_dict['width'] = photo_param['width']
-                    break
+        for photo_param in photo_params_list:            
+            if photo_param['height'] * photo_param['width'] > max_size:
+                max_size = photo_param['height'] * photo_param['width']                
+                photo_params_dict['url'] = photo_param['url']
+                photo_params_dict['size'] = max_size
+            elif photo_param['height'] * photo_param['width'] == 0:
+                photo_params_dict['url'] = None
+                photo_params_dict['size'] = 0
         max_profile_photo_list.append(photo_params_dict)
     return max_profile_photo_list, user_params_list
 
@@ -92,21 +92,22 @@ def upload_files(photos_max_profile_list, users_param_list, n_photos=5):
         photos_max_profile_list = photos_max_profile_list[:n_photos]
 
     for photo_profile in tqdm(photos_max_profile_list):
-        log_file_dict = {}
-        if 'url' in photo_profile:
-            url_photo = photo_profile['url']
-            count_likes = users_param_list.count(photo_profile['user_likes'])
-            if count_likes > 1:
-                timestamp = photo_profile['date']
-                current_date_string = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H-%M-%S')
-                filename = str(photo_profile['user_likes']) + f"_{current_date_string}"
-            else:
-                filename = str(photo_profile['user_likes'])
-            log_file_dict['file_name'] = filename + ".jpg"
-            log_file_dict['size'] = str(photo_profile['width'] * photo_profile['height'])
-            log_upload_files_list.append(log_file_dict)
-            uploader = YaUploader(token=YA_TOKEN)
-            result = uploader.upload_url_to_disk(url_photo, filename)
+        if photo_profile['size'] > 0:
+            log_file_dict = {}
+            if 'url' in photo_profile:
+                url_photo = photo_profile['url']
+                count_likes = users_param_list.count(photo_profile['user_likes'])
+                if count_likes > 1:
+                    timestamp = photo_profile['date']
+                    current_date_string = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H-%M-%S')
+                    filename = str(photo_profile['user_likes']) + f"_{current_date_string}"
+                else:
+                    filename = str(photo_profile['user_likes'])
+                log_file_dict['file_name'] = filename + ".jpg"                
+                log_file_dict['size'] = str(photo_profile['size'])
+                log_upload_files_list.append(log_file_dict)
+                uploader = YaUploader(token=YA_TOKEN)
+                result = uploader.upload_url_to_disk(url_photo, filename)
     sleep(0.1)
     return log_upload_files_list
 
@@ -119,8 +120,7 @@ if __name__ == '__main__':
     # vk_user_id = id_get_request(vk_username)
     count_photos = 5
     user_items_list = photo_get_request(vk_user_id)['response']['items']
-    photo_max_profile_list, user_param_list = get_profile_photos(user_items_list)    
+    photo_max_profile_list, user_param_list = get_profile_photos(user_items_list)
     log_upload_files_json_list = upload_files(photo_max_profile_list, user_param_list, count_photos)
-    
     with open('log_upload_files.json', 'w') as json_file:
         json.dump(log_upload_files_json_list, json_file, ensure_ascii=False, indent=4)
